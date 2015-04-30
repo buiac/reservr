3d853f40e20b182409d2cf24dcaaec91f09c3646
Sebi Kovacs <sebi.kovacs@gmail.com>
2015-04-30 14:01:24 +0300

    Changed date time parsing for emil messages

--- app/controllers/reservations.js
+++ app/controllers/reservations.js
@@ -15,6 +15,8 @@ module.exports = (function(config, db) {
   
   moment.locale('ro');
 
+  console.log(moment().format('dddd, Do MMMM YYYY, HH:mm'));
+
   // set up Mandrill transport
   var transport = nodemailer.createTransport(smtpTransport({
     host: 'smtp.mandrillapp.com',
@@ -28,13 +30,13 @@ module.exports = (function(config, db) {
   // setup text for the user email
   var userEmailSetup = {
     subject: 'Rezervarea a fost facuta',
-    text: 'Salut, \n Ai facut o rezervare de %SEATS% locuri pentru evenimentul "%EVENTNAME%" din data de %EVENTDATE% de la ora %EVENTTIME%. \n Poti modifica oricand rezervarea accesand acest link: %RESERVATIONURL%. \n O zi cat mai buna iti dorim.'
+    text: 'Salut, \n Ai facut o rezervare de %SEATS% locuri pentru evenimentul "%EVENTNAME%" de %EVENTDATE%. \n Poti modifica oricand rezervarea accesand acest link: %RESERVATIONURL%. \n O zi cat mai buna iti dorim.'
   };
 
   // setup text for the owner email
   var ownerEmailSetup = {
     subject: 'O noua rezervare la "%EVENTNAME%"',
-    text: 'Salut, \n O noua rezervare de %SEATS% locuri a fost facuta pentru evenimentul "%EVENTNAME%" din data de %EVENTDATE% de catre %USEREMAIL%. \n O zi cat mai buna iti dorim.'
+    text: 'Salut, \n O noua rezervare de %SEATS% locuri a fost facuta pentru evenimentul "%EVENTNAME%" de %EVENTDATE% de catre %USEREMAIL%. \n O zi cat mai buna iti dorim.'
   };
 
 
@@ -95,8 +97,7 @@ module.exports = (function(config, db) {
             // replace email template variables
             userEmailSetup.text = userEmailSetup.text.replace('%SEATS%', newReservation.seats);
             userEmailSetup.text = userEmailSetup.text.replace('%EVENTNAME%' , theEvent.name);
-            userEmailSetup.text = userEmailSetup.text.replace('%EVENTDATE%' , theEvent.date);
-            userEmailSetup.text = userEmailSetup.text.replace('%EVENTTIME%' , theEvent.date);
+            userEmailSetup.text = userEmailSetup.text.replace('%EVENTDATE%' , moment(theEvent.date).format('dddd, Do MMMM YYYY, HH:mm'));
             userEmailSetup.text = userEmailSetup.text.replace('%RESERVATIONURL%','http://reserver.net/' + theEvent._id + newReservation._id);
 
             // send mail to user
@@ -116,7 +117,7 @@ module.exports = (function(config, db) {
             ownerEmailSetup.subject = ownerEmailSetup.subject.replace('%EVENTNAME%', theEvent.name);
             ownerEmailSetup.text = ownerEmailSetup.text.replace('%SEATS%', newReservation.seats);
             ownerEmailSetup.text = ownerEmailSetup.text.replace('%EVENTNAME%', theEvent.name);
-            ownerEmailSetup.text = ownerEmailSetup.text.replace('%EVENTDATE%', theEvent.date);
+            ownerEmailSetup.text = ownerEmailSetup.text.replace('%EVENTDATE%', moment(theEvent.date).format('dddd, Do MMMM YYYY, HH:mm'));
             ownerEmailSetup.text = ownerEmailSetup.text.replace('%USEREMAIL%', newReservation.email);
 
             // send a mail to venue owner
